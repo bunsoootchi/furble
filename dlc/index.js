e9297cbec673df266371319ae95a9d2c4d351d0b
bunsoootchi <bunsoootchi@gmail.com>
2024-07-25 20:28:34 +0800

    Added DLCs and more

---
+++ dlc/index.js
@@ -0,0 +1,247 @@
+[
+    {
+        "file": "tu011090.dlc", 
+        "title": "Songs Pack 1",
+        "buttons": [
+            { "title": "WDTA1", "action": [75,0,2,1] },
+            { "title": "WDTA2", "action": [75,0,2,2] },
+            { "title": "WDTA3", "action": [75,0,2,3] },
+            { "title": "WDTA4", "action": [75,0,2,4] },
+            { "title": "WDTA5", "action": [75,0,2,5] },
+            { "title": "WDTA6", "action": [75,0,2,6] },
+            { "title": "WDTA7", "action": [75,0,2,7] },
+            { "title": "PR1", "action": [75,0,1,1] },
+            { "title": "PR2", "action": [75,0,1,2] },
+            { "title": "PR3", "action": [75,0,1,3] },
+            { "title": "PR4", "action": [75,0,1,4] },
+            { "title": "PR5", "action": [75,0,1,5] },
+            { "title": "PR6", "action": [75,0,1,6] },
+            { "title": "PR7", "action": [75,0,1,7] },
+            { "title": "MSN1", "action": [75,0,4,1] },
+            { "title": "MSN2", "action": [75,0,4,2] },
+            { "title": "MSN3", "action": [75,0,4,3] },
+            { "title": "MSN4", "action": [75,0,4,4] },
+            { "title": "MSN5", "action": [75,0,4,5] },
+            { "title": "MSN6", "action": [75,0,4,6] },
+            { "title": "MSN7", "action": [75,0,4,7] }
+        ]
+    },
+    {
+        "file": "tu012750.dlc", 
+        "title": "Songs Pack 2",
+        "buttons": [
+            { "title": "UCTT1", "action": [75,0,1,1] },
+            { "title": "UCTT2", "action": [75,0,1,2] },
+            { "title": "UCTT3", "action": [75,0,1,3] },
+            { "title": "UCTT4", "action": [75,0,1,4] },
+            { "title": "UCTT5", "action": [75,0,1,5] },
+            { "title": "UCTT6", "action": [75,0,1,6] },
+            { "title": "UCTT7", "action": [75,0,1,7] },
+            { "title": "JBR1", "action": [75,0,2,1] },
+            { "title": "JBR2", "action": [75,0,2,2] },
+            { "title": "JBR3", "action": [75,0,2,3] },
+            { "title": "JBR4", "action": [75,0,2,4] },
+            { "title": "JBR5", "action": [75,0,2,5] },
+            { "title": "JBR6", "action": [75,0,2,6] },
+            { "title": "JBR7", "action": [75,0,2,7] },
+            { "title": "D1", "action": [75,0,4,1] },
+            { "title": "D2", "action": [75,0,4,2] },
+            { "title": "D3", "action": [75,0,4,3] },
+            { "title": "D4", "action": [75,0,4,4] },
+            { "title": "D5", "action": [75,0,4,5] },
+            { "title": "D6", "action": [75,0,4,6] },
+            { "title": "D7", "action": [75,0,4,7] }
+        ]
+    },
+    {
+        "file": "tu012770.dlc", 
+        "title": "Songs Pack 3",
+        "buttons": [
+            { "title": "SIY1", "action": [75,0,1,1] },
+            { "title": "SIY2", "action": [75,0,1,2] },
+            { "title": "SIY3", "action": [75,0,1,3] },
+            { "title": "SIY4", "action": [75,0,1,4] },
+            { "title": "SIY5", "action": [75,0,1,5] },
+            { "title": "SIY6", "action": [75,0,1,6] },
+            { "title": "SIY7", "action": [75,0,1,7] },
+            { "title": "CMM1", "action": [75,0,2,1] },
+            { "title": "CMM2", "action": [75,0,2,2] },
+            { "title": "CMM3", "action": [75,0,2,3] },
+            { "title": "CMM4", "action": [75,0,2,4] },
+            { "title": "CMM5", "action": [75,0,2,5] },
+            { "title": "CMM6", "action": [75,0,2,6] },
+            { "title": "CMM7", "action": [75,0,2,7] },
+            { "title": "RT1", "action": [75,0,4,1] },
+            { "title": "RT2", "action": [75,0,4,2] },
+            { "title": "RT3", "action": [75,0,4,3] },
+            { "title": "RT4", "action": [75,0,4,4] },
+            { "title": "RT5", "action": [75,0,4,5] },
+            { "title": "RT6", "action": [75,0,4,6] },
+            { "title": "RT7", "action": [75,0,4,7] }
+        ]
+    },
+    {
+        "file": "tu011840.dlc",
+        "title": "Songs Pack 4",
+        "buttons": [
+            { "title": "BTE1", "action": [75, 0, 1, 1] },
+            { "title": "BTE2", "action": [75, 0, 1, 2] },
+            { "title": "BTE3", "action": [75, 0, 1, 3] }, 
+            { "title": "BTE4", "action": [75, 0, 1, 4] }, 
+            { "title": "BTE5", "action": [75, 0, 1, 5] }, 
+            { "title": "BTE6", "action": [75, 0, 1, 6] }, 
+            { "title": "BTE7", "action": [75, 0, 1, 7] }, 
+            { "title": "Stay1", "action": [75, 0, 2, 1] },
+            { "title": "Stay2", "action": [75, 0, 2, 2] },
+            { "title": "Stay3", "action": [75, 0, 2, 3] },
+            { "title": "Stay4", "action": [75, 0, 2, 4] },
+            { "title": "Stay5", "action": [75, 0, 2, 5] },
+            { "title": "Stay6", "action": [75, 0, 2, 6] },
+            { "title": "Stay7", "action": [75, 0, 2, 7] },
+            { "title": "Hanazuki1", "action": [75, 0, 3, 1] },
+            { "title": "Hanazuki2", "action": [75, 0, 3, 2] },
+            { "title": "Hanazuki3", "action": [75, 0, 3, 3] },
+            { "title": "Hanazuki4", "action": [75, 0, 3, 4] },
+            { "title": "Hanazuki5", "action": [75, 0, 3, 5] },
+            { "title": "Hanazuki6", "action": [75, 0, 3, 6] },
+            { "title": "Hanazuki7", "action": [75, 0, 3, 7] },
+            { "title": "CMM1", "action": [75, 0, 4, 1] },
+            { "title": "CMM2", "action": [75, 0, 4, 2] },
+            { "title": "CMM3", "action": [75, 0, 4, 3] },
+            { "title": "CMM4", "action": [75, 0, 4, 4] },
+            { "title": "CMM5", "action": [75, 0, 4, 5] },
+            { "title": "CMM6", "action": [75, 0, 4, 6] },
+            { "title": "CMM7", "action": [75, 0, 4, 7] }
+        ]
+    },
+    {
+        "file": "tu011880.dlc",
+        "title": "Songs Pack 5",
+        "buttons": [
+            { "title": "CMM1", "action": [75, 0, 1, 1] },
+            { "title": "CMM2", "action": [75, 0, 1, 2] },
+            { "title": "CMM3", "action": [75, 0, 1, 3] }, 
+            { "title": "CMM4", "action": [75, 0, 1, 4] }, 
+            { "title": "CMM5", "action": [75, 0, 1, 5] }, 
+            { "title": "CMM6", "action": [75, 0, 1, 6] }, 
+            { "title": "CMM7", "action": [75, 0, 1, 7] }, 
+            { "title": "TheSign1", "action": [75, 0, 2, 1] },
+            { "title": "TheSign2", "action": [75, 0, 2, 2] },
+            { "title": "TheSign3", "action": [75, 0, 2, 3] },
+            { "title": "TheSign4", "action": [75, 0, 2, 4] },
+            { "title": "TheSign5", "action": [75, 0, 2, 5] },
+            { "title": "TheSign6", "action": [75, 0, 2, 6] },
+            { "title": "TheSign7", "action": [75, 0, 2, 7] },
+            { "title": "bday", "action": [75, 0, 3, 1] },
+            { "title": "cake2", "action": [75, 0, 3, 2] },
+            { "title": "cake3", "action": [75, 0, 3, 3] },
+            { "title": "surprise", "action": [75, 0, 3, 4] },
+            { "title": "sorryNot", "action": [75, 0, 3, 5] },
+            { "title": "ilovecake", "action": [75, 0, 3, 6] },
+            { "title": "bdaycake7", "action": [75, 0, 3, 7] },
+            { "title": "gangnam1", "action": [75, 0, 4, 1] },
+            { "title": "gangnam2", "action": [75, 0, 4, 2] },
+            { "title": "gangnam3", "action": [75, 0, 4, 3] },
+            { "title": "gangnam4", "action": [75, 0, 4, 4] },
+            { "title": "gangnam5", "action": [75, 0, 4, 5] },
+            { "title": "gangnam6", "action": [75, 0, 4, 6] },
+            { "title": "gangnam7", "action": [75, 0, 4, 7] }
+        ]
+    },    
+    {
+        "file": "tu022060.dlc",
+        "title": "Songs Pack 6",
+        "buttons": [
+            { "title": "ding", "action": [75, 0, 2, 3] },
+            { "title": "italian", "action": [75, 0, 2, 4] },
+            { "title": "wow big", "action": [75, 0, 2, 5] },
+            { "title": "so peaceful", "action": [75, 0, 3, 2] },
+            { "title": "sniff", "action": [75, 0, 3, 3] },
+            { "title": "tickled", "action": [75, 0, 3, 4] },
+            { "title": "evil laugh", "action": [75, 0, 3, 5] },
+            { "title": "newvideo", "action": [75, 0, 5, 0] },
+            { "title": "cccorgi", "action": [75, 0, 0, 3] },
+            { "title": "beatbox", "action": [75, 0, 0, 4] },
+            { "title": "too cool", "action": [75, 0, 0, 5] },
+            { "title": "love corgi", "action": [75, 0, 0, 7] },
+            { "title": "unsteady1", "action": [75, 0, 1, 2] },
+            { "title": "unsteady2", "action": [75, 0, 1, 3] },
+            { "title": "unsteady3", "action": [75, 0, 1, 4] },
+            { "title": "unsteady4", "action": [75, 0, 1, 5] },
+            { "title": "bestsongever1", "action": [75, 0, 4, 2] },
+            { "title": "bestsongever2", "action": [75, 0, 4, 3] },
+            { "title": "bestsongever3", "action": [75, 0, 4, 4] }
+        ]
+    },    
+    {
+        "file": "Orks.dlc", 
+        "title": "Orks",
+        "buttons": [
+            { "title": "Be Green", "action": [75,0,4,1] },
+            { "title": "Waaagh", "action": [75,0,3,2] },
+            { "title": "Boss", "action": [75,0,4,0] },
+            { "title": "Pee", "action": [75,0,6,5] },
+            { "title": "Listen", "action": [75,0,5,6] },
+            { "title": "Spit", "action": [75,0,6,6] },
+            { "title": "Snotling", "action": [75,0,4,5] },
+            { "title": "Top Ork", "action": [75,0,4,6] },
+            { "title": "Snivel", "action": [75,0,7,6] },
+            { "title": "Void", "action": [75,0,0,3] },
+            { "title": "Orks", "action": [75,0,0,2] },
+            { "title": "Green iz best", "action": [75,0,4,7] },
+            { "title": "In Charge", "action": [75,0,3,3] } 
+        ]
+    },
+    {
+        "file": "Waaagh.dlc", 
+        "title": "Waaaagh",
+        "buttons": [
+            { "title": "WAAAAAAGH!", "action": [75,0,0,2] }
+        ]
+    },
+    
+    {
+        "file": "EvilSeed.dlc", 
+        "title": "Evil Seed",
+        "buttons": [
+            { "title": "Warning Long Song", "action": [75,0,0,2] }
+        ]
+    },
+    { 
+        "file": "SciFix.dlc", 
+        "title": "Science!",
+        "buttons": [
+            { "title": "FUCK MY HOUSE!", "action": [75,0,0,2] }
+         ]
+    },
+    {
+        "file": "chilli.dlc", 
+        "title": "Chilli",
+        "buttons": [
+            { "title": "Just Chilli Eyes ", "action": [75,0,4,4] }
+        ]
+    },
+    
+    { 
+        "file": "context.dlc", 
+        "title": "Context Logo",
+        "buttons": [
+            { "title": "Logo", "action": [75,0,4,4] }
+        ]
+    },
+    { 
+        "file": "hacked.dlc", 
+        "title": "Daleks Invasion",
+        "buttons": [
+            { "title": "Exterminate", "action": [75,0,3,4] },
+            { "title": "Kill all Humans", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "noimg2.dlc", 
+        "title": "Audio only",
+        "buttons": [
+            { "title": "Audio test ", "action": [75,0,4,4] }
+        ]
+    }
+]
